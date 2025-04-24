a82fe4eaa97ba1ca392fbdcf4691fb77b3f01bf3
daneshwarihavalad <daneshwarisudheerhavalad@gmail.com>
2025-04-24 11:34:55 +0530

    savethis

--- demo.java
+++ demo.java
@@ -6,13 +6,21 @@ public class demo {
         public static void main(String[] args) {
             int a = 3;
             int b = 8;
-            int x, y;
+            int x, y, z, p;
     
             x = a + b;  
             y = a - b;  
+            z = a * b;
+            p = a / b;
+
     
             System.out.println("Addition is " + x);
             System.out.println("Subtraction is " + y);  
+            System.out.println("Multiplication is " + z);  
+            System.out.println("Division is " + p);  
+
+
+
         }
     }
     
