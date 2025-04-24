43fa930f94a26ba1832afb56406ee1e65b3c74a4
daneshwarihavalad <daneshwarisudheerhavalad@gmail.com>
2025-04-24 12:56:25 +0530

    saved

--- demo.java
+++ demo.java
@@ -1,16 +1,13 @@
-package java;
 
-public class demo {
-    
 
-        public static void main(String[] args) {
+public class Demo {
+            public static void main(String[] args) {
             int a = 6;
             int b = 8;
             int x, y;
     
             x = a + b;  
             y = a - b;  
-    
             System.out.println("Addition is " + x);
             System.out.println("Subtraction is " + y);  
         }
