f3ac201a9ad496502191fc58d5e3efcb26010903
daneshwarihavalad <daneshwarisudheerhavalad@gmail.com>
2025-04-24 11:23:18 +0530

    savethis

---
+++ demo.java
@@ -0,0 +1,20 @@
+package java;
+
+public class demo {
+    
+
+        public static void main(String[] args) {
+            int a = 6;
+            int b = 8;
+            int x, y;
+    
+            x = a + b;  
+            y = a - b;  
+    
+            System.out.println("Addition is " + x);
+            System.out.println("Subtraction is " + y);  
+        }
+    }
+    
+    
+
